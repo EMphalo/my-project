e1f840eedcda772a145bca5c448fe799107fb172
emphalo <mphaloemmanuel@gmail.com>
2024-06-04 10:48:57 +0200

    #tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p class="text-blue-900 shadow-lg mt-10 p-20 bg-green-200 text-3xl font-bold">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
