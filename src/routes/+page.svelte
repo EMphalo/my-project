4b481b9ee11cb227ecec0d2963896e456e2cf294
emphalo <mphaloemmanuel@gmail.com>
2024-06-04 16:15:40 +0200

    #tailwind css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p class="text-blue-900 shadow-lg mt-10 p-20 bg-green-200 text-3xl font-bold">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+  
+  <slot />
+<nav class="bg-orange-200 text-black py-2">
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+</nav>
+
+<h1>Home Page</h1>
+<p>This is the landing page</p>
+<div class="flex justify-center">
+    <div class="">
+      <!-- Card 1 -->
+      <div class="border-r-4 border-yellow-600 border-x-yellow-600  mt-20 mx-auto w-2/6 p-5 text-zinc-900 text-3xl bg-gradient-to-br from-orange-400 to-green-600 rounded-lg  shadow-md shadow-slate-200/50">
+        <!-- Card content -->
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Eius ratione vel ipsam beatae dolore unde! Quia cum, cumque, sint
+             consequuntur rerum, ipsam veniam incidunt eum sequi ratione voluptas? 
+             Consequuntur, itaque!
+        </p>
+
+      </div>
+
+    </div>
+  </div>
+  
+
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,7 @@
+<nav class="bg-orange-200 text-black py-2">
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+</nav>
+
+<h1>About Page</h1>
+<p>This is the about page</p>
