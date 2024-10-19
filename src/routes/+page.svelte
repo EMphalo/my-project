e7e255bae17889f113f7dc270ebd443ddec12148
EMphalo <111180890+EMphalo@users.noreply.github.com>
2024-10-19 22:24:21 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,10 +6,7 @@
       <div class="hover:invert border-r-4 border-yellow-600 border-x-yellow-600  mt-20 mx-auto w-2/6 p-5 text-zinc-900 text-3xl bg-gradient-to-br from-orange-400 to-green-600 rounded-lg  shadow-md shadow-slate-200/50">
         <!-- Card content -->
         <p class="">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-            Eius ratione vel ipsam beatae dolore unde! Quia cum, cumque, sint
-            consequuntur rerum, ipsam veniam incidunt eum sequi ratione voluptas? 
-            Consequuntur, itaque!
+           Welcome to my ste
         </p>
 
       </div>
